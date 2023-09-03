eafd4fcfe253bc9987e1b2c99e84226cc94fb7d3
MariaXMora <mximemc@hotmail.com>
2023-09-03 13:30:22 -0500

    primera save

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import { valida } from "./validaciones.js";
+
+const input = document.querySelectorAll('input');
+
+input.forEach((input) => {
+    input.addEventListener('blur', (input) => {
+      valida(input.target);
+    });
+  });
+
